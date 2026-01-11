426700dae2293888ed9aee3798d52a78e57264af
mintesnotderib <mentesnot.derib@gmail.com>
2026-01-11 14:26:05 +0300

    Revert "first trying in merging"
    This reverts commit 575ab3075b288a8d435effde83eae7a2c58ba552.

--- b.js
+++ b.js
@@ -1,18 +1,1 @@
 console.log("this is my trying to know  about the git hub");
-console.log("this is not only try but this also effort");
-/*
-git status
-git init
-git commit -m "this is my commit"
-git push -u origin main
-git add origin https://github.com/mintesnotderib/personal-profile.git
-git push origin main
-git branch
-git checkout -b mybranch
-git add .
-git clone https://github.com/mintesnotderib/personal-profile.git
- git config --global user.email "me.com"
-  git config --global user.name "Your Name"
-  git checkout branchname
-  git merge otherbrancehname
-*/
